a1b1c74d2d69345f9eec1f74042b25a94533d4bd
UhaYuss <94.hoangquan.toky@gmail.com>
2024-10-29 17:53:22 +0700

    Design sidebar

---
+++ WinFormCSM/GUI/CustomControls/SideBar.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace GUI.CustomControls
+{
+    partial class SideBar
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Button button1;
+            Button button2;
+            Button button3;
+            Button button4;
+            Button button5;
+            Button button6;
+            Button button7;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SideBar));
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.BackColor = Color.FromArgb(35, 40, 45);
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Controls.Add(button7, 0, 6);
+            tableLayoutPanel1.Controls.Add(button6, 0, 5);
+            tableLayoutPanel1.Controls.Add(button5, 0, 4);
+            tableLayoutPanel1.Controls.Add(button4, 0, 3);
+            tableLayoutPanel1.Controls.Add(button3, 0, 2);
+            tableLayoutPanel1.Controls.Add(button2, 0, 1);
+            tableLayoutPanel1.Controls.Add(button1, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(3, 166);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 8;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Size = new Size(188, 562);
+            tableLayoutPanel1.TabIndex = 2;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.BackColor = Color.FromArgb(35, 40, 45);
+            tableLayoutPanel2.ColumnCount = 1;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel2.Controls.Add(pictureBox1, 0, 0);
+            tableLayoutPanel2.Controls.Add(tableLayoutPanel1, 0, 1);
+            tableLayoutPanel2.ForeColor = SystemColors.ActiveCaptionText;
+            tableLayoutPanel2.Location = new Point(3, 6);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 2;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle());
+            tableLayoutPanel2.RowStyles.Add(new RowStyle());
+            tableLayoutPanel2.Size = new Size(194, 731);
+            tableLayoutPanel2.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(188, 157);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(35, 40, 45);
+            button1.BackgroundImageLayout = ImageLayout.None;
+            button1.CausesValidation = false;
+            button1.Dock = DockStyle.Fill;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.Control;
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(182, 55);
+            button1.TabIndex = 0;
+            button1.Text = "Trang Chủ";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(35, 40, 45);
+            button2.BackgroundImageLayout = ImageLayout.None;
+            button2.CausesValidation = false;
+            button2.Dock = DockStyle.Fill;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.Control;
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(3, 64);
+            button2.Name = "button2";
+            button2.Size = new Size(182, 55);
+            button2.TabIndex = 1;
+            button2.Text = "Sản Phẩm";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(35, 40, 45);
+            button3.BackgroundImageLayout = ImageLayout.None;
+            button3.CausesValidation = false;
+            button3.Dock = DockStyle.Fill;
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.ForeColor = SystemColors.Control;
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(3, 125);
+            button3.Name = "button3";
+            button3.Size = new Size(182, 55);
+            button3.TabIndex = 2;
+            button3.Text = "Bán Hàng";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(35, 40, 45);
+            button4.BackgroundImageLayout = ImageLayout.None;
+            button4.CausesValidation = false;
+            button4.Dock = DockStyle.Fill;
+            button4.FlatStyle = FlatStyle.Popup;
+            button4.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.ForeColor = SystemColors.Control;
+            button4.Image = (Image)resources.GetObject("button4.Image");
+            button4.ImageAlign = ContentAlignment.MiddleLeft;
+            button4.Location = new Point(3, 186);
+            button4.Name = "button4";
+            button4.Size = new Size(182, 55);
+            button4.TabIndex = 3;
+            button4.Text = "Thống kê";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(35, 40, 45);
+            button5.BackgroundImageLayout = ImageLayout.None;
+            button5.CausesValidation = false;
+            button5.Dock = DockStyle.Fill;
+            button5.FlatStyle = FlatStyle.Popup;
+            button5.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button5.ForeColor = SystemColors.Control;
+            button5.Image = (Image)resources.GetObject("button5.Image");
+            button5.ImageAlign = ContentAlignment.MiddleLeft;
+            button5.Location = new Point(3, 247);
+            button5.Name = "button5";
+            button5.Size = new Size(182, 55);
+            button5.TabIndex = 4;
+            button5.Text = "Nhân Viên";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(35, 40, 45);
+            button6.BackgroundImageLayout = ImageLayout.None;
+            button6.CausesValidation = false;
+            button6.Dock = DockStyle.Fill;
+            button6.FlatStyle = FlatStyle.Popup;
+            button6.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button6.ForeColor = SystemColors.Control;
+            button6.Image = (Image)resources.GetObject("button6.Image");
+            button6.ImageAlign = ContentAlignment.MiddleLeft;
+            button6.Location = new Point(3, 308);
+            button6.Name = "button6";
+            button6.Size = new Size(182, 55);
+            button6.TabIndex = 5;
+            button6.Text = "Khách hàng";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.FromArgb(35, 40, 45);
+            button7.BackgroundImageLayout = ImageLayout.None;
+            button7.CausesValidation = false;
+            button7.Dock = DockStyle.Fill;
+            button7.FlatStyle = FlatStyle.Popup;
+            button7.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button7.ForeColor = SystemColors.Control;
+            button7.Image = (Image)resources.GetObject("button7.Image");
+            button7.ImageAlign = ContentAlignment.MiddleLeft;
+            button7.Location = new Point(3, 369);
+            button7.Name = "button7";
+            button7.Size = new Size(182, 55);
+            button7.TabIndex = 6;
+            button7.Text = "Đăng xuất";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // SideBar
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(tableLayoutPanel2);
+            Name = "SideBar";
+            Size = new Size(208, 740);
+            Load += SideBar_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ WinFormCSM/GUI/Views/DonHang/Index.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace GUI.Views.DonHang
+{
+    partial class Index
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Index
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1026, 481);
+            Name = "Index";
+            Text = "Index";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
